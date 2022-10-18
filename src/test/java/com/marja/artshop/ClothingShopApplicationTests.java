5ba551a88ee0bdde59fa938202104c58ee804518
Marja <murrha@outlook.com>
2022-10-17 21:26:49 -0700

    Added in attributes for User model

--- src/test/java/com/marja/artshop/ClothingShopApplicationTests.java
+++ src/test/java/com/marja/artshop/ClothingShopApplicationTests.java
@@ -1,13 +1,23 @@
 package com.marja.artshop;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+import com.marja.clothingshop.repo.CategoryRepository;
+import com.marja.clothingshop.repo.ProductRepository;
 
 @SpringBootTest
 class ClothingShopApplicationTests {
 
+	private CategoryRepository categoryRepository;
+
+	private ProductRepository productRepository; 
+	
 	@Test
-	void contextLoads() {
+	void getAllCategories() {
+		
 	}
 
 }
